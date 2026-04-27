625147618a8864d96e027fa79ad1bf76c413069b
AshutoshH <hagawaneashutosh1@gmail.com>
2026-04-28 00:16:29 +0530

    learnt Basic class-object structure

---
+++ Class_Demo.js
@@ -0,0 +1,27 @@
+class Product{
+
+    // Data members
+    name;
+    price;
+    category;
+    description;
+    rating;
+
+    // Member functions
+    addToCart(){
+        console.log("Product added to cart");
+    }
+
+    removefromcart(){
+        console.log("Product removed from cart");
+    }
+
+    displayProduct(){
+        console.log("Product displayed");
+    }
+
+    buyProduct(){
+        console.log("Product bought");
+        
+    }
+}
