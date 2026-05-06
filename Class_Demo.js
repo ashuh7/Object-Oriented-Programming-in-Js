44d6b5d52b886e38d9aa97372541daf12df06e44
AshutoshH <hagawaneashutosh1@gmail.com>
2026-05-06 23:14:18 +0530

    Added Getter Setter Member functions code

--- Class_Demo.js
+++ Class_Demo.js
@@ -1,13 +1,43 @@
 class Product{
 
     // Data members
-    name;
-    price;
+    #name;
+    #price;
     category;
     description;
     rating;
-
+    constructor(productName, productPrice, productCategory, productDescription, productRating){
+        if(productName === undefined || productPrice === undefined || productCategory === undefined
+            || productDescription === undefined || productRating === undefined){
+                console.log("Invalid parameters");
+                return;
+            }
+        
+        this.#name = productName;
+        if(productPrice > 0 && typeof(productPrice) === "number"){
+            this.#price = productPrice;
+        }
+        this.category = productCategory;
+        this.description = productDescription;
+        this.rating = productRating;
+    }
     // Member functions
+
+    // getter functions
+    getPrice(){
+        return this.#price;
+    }
+
+    //setter functions
+    setPrice(p){
+        if(p>0){
+            this.#price = p;
+        }else{
+            console.log("Invalid Price");
+            
+        }
+    }
+
     addToCart(){
         console.log("Product added to cart");
     }
@@ -17,7 +47,7 @@ class Product{
     }
 
     displayProduct(){
-        console.log("Product displayed");
+        console.log("Product displayed", this);
     }
 
     buyProduct(){
@@ -25,3 +55,14 @@ class Product{
         
     }
 }
+
+let iphone = new Product();
+console.log(iphone);
+
+iphone.name = 'Iphone 12';
+// iphone.price = 25000;
+iphone.category = "Electronics";
+
+iphone.setPrice(35000); // setting new Iphone price using setter method
+
+iphone.displayProduct();
